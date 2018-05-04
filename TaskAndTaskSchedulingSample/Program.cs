6f4205695b221c35a5632463351ad511215eb7ee
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-05-04 17:38:37 +0800

    Task a = new Task()

--- TaskAndTaskSchedulingSample/Program.cs
+++ TaskAndTaskSchedulingSample/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace TaskAndTaskSchedulingSample
+public class Example
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   public static void Main()
+   {
+      Thread.CurrentThread.Name = "Main";
+
+      // Create a task and supply a user delegate by using a lambda expression. 
+      Task taskA = new Task( () => Console.WriteLine("Hello from taskA."));
+      // Start the task.
+      taskA.Start();
+
+      // Output a message from the calling thread.
+      Console.WriteLine("Hello from thread '{0}'.", 
+                        Thread.CurrentThread.Name);
+      taskA.Wait();
+   }
 }
+// The example displays output like the following:
+//       Hello from thread 'Main'.
+//       Hello from taskA.
