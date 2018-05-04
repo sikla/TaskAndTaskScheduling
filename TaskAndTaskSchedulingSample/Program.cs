1e295b673952dcf6c651f644d44fc250e83455d0
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-05-04 17:22:27 +0800

    blank project

---
+++ TaskAndTaskSchedulingSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskAndTaskSchedulingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
